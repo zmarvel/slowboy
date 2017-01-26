39a31bb67691e9bff51175a3f249255e0680c518
Zack Marvel <zpmarvel@gmail.com>
2017-01-25 22:34:46 -0500

    somewhat bare setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+
+from distutils.core import setup
+
+setup(  name='slowboy',
+        version='0.0.1',
+        packages=['slowboy'],
+        url='https://github.com/zmarvel/slowboy/',
+        author='Zack Marvel',
+        author_email='zpmarvel at gmail dot com')

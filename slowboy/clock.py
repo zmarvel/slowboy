77b1d0ecf18531f1b017c6bd7689695e6bbe1ae6
Zack Marvel <zpmarvel@gmail.com>
2017-02-01 23:30:17 -0500

    Empty clock class

---
+++ slowboy/clock.py
@@ -0,0 +1,16 @@
+
+
+class Clock():
+    """The implementation of instructions should take care of incrementing the
+    clock appropriately."""
+
+    def __init__(self):
+        pass
+
+    def tick(self):
+        # increment the clock
+        # sleep? to maintain CPU freq
+        pass
+
+    def tickby(self, n):
+        pass

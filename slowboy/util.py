3ad44319bc828601e364d51312b6b1e30bc67bcf
Zack Marvel <zpmarvel@gmail.com>
2017-02-14 17:12:23 -0500

    Added two's complement helpers to util
    TODO test these, replace instances where I have manually written it out
    if it doesn't greatly impact performance and it improves readability.

--- slowboy/util.py
+++ slowboy/util.py
@@ -7,3 +7,33 @@ def uint8toBCD(uint8):
     d0 = uint8 % 10
 
     return (d1 << 4) | d0
+
+def sub_s8(x, y):
+    """Subtract two 8-bit integers stored in two's complement."""
+
+    return (x + twoscompl8(y)) & 0xff
+
+def sub_s16(x, y):
+    """Subtract two 16-bit integers stored in two's complement."""
+
+    return (x + twoscompl16(y)) & 0xffff
+
+def add_s8(x, y):
+    """Add two 8-bit integers stored in two's complement."""
+
+    return (x + y) & 0xff
+
+def add_s16(x, y):
+    """Add two 16-bit integers stored in two's complement."""
+
+    return (x + y) & 0xffff
+
+def twoscompl8(x):
+    """Returns the reciprocal of 8-bit x in two's complement."""
+
+    return ((x ^ 0xff) + 1) & 0xff
+
+def twoscompl16(x):
+    """Returns the reciprocal of 16-bit x in two's complement."""
+
+    return ((x ^ 0xffff) + 1) & 0xffff
